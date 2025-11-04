22bfab600c6dc9fc878f06da7ff89b6effb7f711
THEJAS-BK <thejasbk998@gmail.com>
2025-11-04 16:04:37 +0000

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,6 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div></div>
+    <div>4</div>
   </React.StrictMode>
 );
