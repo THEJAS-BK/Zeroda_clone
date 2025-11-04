6b234a5cbb0b21a498a9e2badc44ee307343a0e2
THEJAS-BK <thejasbk998@gmail.com>
2025-11-04 16:13:08 +0000

    Changes for project-structure branch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,6 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div>4</div>
+    <div></div>
   </React.StrictMode>
 );
