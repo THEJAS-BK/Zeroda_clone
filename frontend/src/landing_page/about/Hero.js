e2e5811365d411341f9d465279d618f3e04394e5
THEJAS-BK <thejasbk998@gmail.com>
2025-12-10 17:12:47 +0000

    changes

--- frontend/src/landing_page/about/Hero.js
+++ frontend/src/landing_page/about/Hero.js
@@ -1,41 +1,56 @@
-import {Link} from "react-router-dom"
 function Hero() {
   return (
     <div className="container">
       <div className="row">
-        <h1 className="col-6 offset-3" style={{fontSize:"1.55rem",marginTop:"6rem",marginBottom:"5rem"}}>
+        <h1
+          className="col-6 offset-3 "
+          style={{
+            fontSize: "1.55rem",
+            marginTop: "6rem",
+            marginBottom: "7rem",
+          }}
+        >
           We pioneered the discount broking model in India. Now, we are breaking
           ground with our technology.
         </h1>
         <hr/>
-        <p className="mt-5">
-          We kick-started operations on the 15th of August, 2010 with the goal
-          of breaking all barriers that traders and investors face in India in
-          terms of cost, support, and technology. We named the company Zerodha,
-          a combination of Zero and "Rodha", the Sanskrit word for barrier.
-        </p>
-        <p>
-          Today, our disruptive pricing models anda in-house technology have made
-          us the biggest stock broker in India.
-        </p>
-        <p>
-          Over 1.6+ crore clients place billions of orders every year through
-          our powerful ecosystem of investment platforms, contributing over 15%
-          of all Indian retail trading volumes.
-        </p>
-        <p>
-          In addition, we run a number of popular open online educational and
-          community initiatives to empower retail traders and investors.
-        </p>
-        <p>
-          <Link to="/" className="anchorTag">Rainmatter</Link>, our fintech fund and incubator, has invested in several
-          fintech startups with the goal of growing the Indian capital markets.
-        </p>
-        <p>
-          And yet, we are always up to something new every day. Catch up on the
-          latest updates on our blog or see what the media is saying about us or
-          learn more about our business and product philosophies.
-        </p>
+        <div className="row" style={{marginTop:"5rem"}}>
+          <div className="col-4 offset-2">
+             <p className="paraText">
+              We kick-started operations on the 15th of August, 2010 with the
+              goal of breaking all barriers that traders and investors face in
+              India in terms of cost, support, and technology. We named the
+              company Zerodha, a combination of Zero and "Rodha", the Sanskrit
+              word for barrier.
+            </p>
+             <p className="paraText">
+              Today, our disruptive pricing models and in-house technology have
+              made us the biggest stock broker in India.
+            </p>
+             <p className="paraText">
+              Over 1.6+ crore clients place billions of orders every year
+              through our powerful ecosystem of investment platforms,
+              contributing over 15% of all Indian retail trading volumes.
+            </p>
+          </div>
+          <div className="col-4">
+             <p className="paraText">
+              In addition, we run a number of popular open online educational
+              and community initiatives to empower retail traders and investors.
+            </p>
+             <p className="paraText">
+              <a href="/" className="anchorTag">Rainmatter</a>, our fintech fund and incubator, has invested in
+              several fintech startups with the goal of growing the Indian
+              capital markets.
+            </p>
+             <p className="paraText">
+              And yet, we are always up to something new every day. Catch up on
+              the latest updates on our blog or see what the media is saying
+              about us or learn more about our business and product
+              philosophies.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -1,6 +1,101 @@
-import React from "react";
+import People from "./People";
+
 function Team() {
-  return <h1>Team</h1>;
+  return (
+    <div className="container" style={{ marginTop: "5rem" }}>
+      <h3 style={{ textAlign: "center", opacity: "0.95" }}>People</h3>
+      <div className="row mt-5">
+        <div className="col-4 offset-1 d-flex justify-content-end">
+          <People
+            person={"/media/images/nithinKamath.jpg"}
+            name={"Nithin Kamath"}
+            designation="Founder, CEO"
+          />
+        </div>
+        <div className="col-5 ms-5">
+          <p className="paraText">
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry.
+          </p>
+          <p className="paraText">
+            He is a member of the SEBI Secondary Market Advisory Committee
+            (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+          <p className="paraText">Playing basketball is his zen.</p>
+          <p className="paraText">
+            Connect on
+            <a className="anchorTag ms-2" href="/" alt="homepage">
+              Homepage
+            </a>
+            /
+            <a className="anchorTag ms-2" href="/" alt="homepage">
+              TradingQnA
+            </a>
+            /
+            <a className="anchorTag ms-2" href="/" alt="Twitter">
+              Twitter
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="row" style={{ marginLeft: "3rem",marginTop:"7rem" }}>
+        <div className="col-4">
+          <People
+            person={"/media/images/NikhilKamath.jpg"}
+            name={"Nikil Kamath"}
+            designation={"co-founder & CFO"}
+          />
+        </div>
+        <div className="col-4">
+          <People
+            person={"/media/images/Kailash.jpg"}
+            name={"Dr. Kailash Nadh"}
+            designation={"CTO"}
+          />
+        </div>
+        <div className="col-4">
+          <People
+            person={"/media/images/Venu.jpg"}
+            name={"Venu Madhav"}
+            designation={"COO"}
+          />
+        </div>
+      </div>
+      <div className="row" style={{ marginLeft: "3rem" }}>
+        <div className="col-4">
+          <People
+            person={"/media/images/Hanan.jpg"}
+            name={"Hanan Delvi"}
+            designation={"CCO"}
+          />
+        </div>
+        <div className="col-4">
+          <People
+            person={"/media/images/Seema.jpg"}
+            name={"Seema Patil"}
+            designation={"Director"}
+          />
+        </div>
+        <div className="col-4">
+          <People
+            person={"/media/images/karthik.jpg"}
+            name={"Karthik Rangappa"}
+            designation={"Chief of Education"}
+          />
+        </div>
+      </div>
+       <div className="row" style={{ marginLeft: "3rem" }}>
+        <div className="col-4">
+          <People
+            person={"/media/images/Austin.jpg"}
+            name={"Austin Prakesh"}
+            designation={"Director Strategy"}
+          />
+          </div>
+      </div>
+    </div>
+  );
 }
 
 export default Team;
